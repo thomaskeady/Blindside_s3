c8a52f5db4e5a65fef63e539c8ea495347685da6
thomaskeady <thomas.keady@jhu.edu>
2017-10-02 11:19:17 -0400

    Plan for MLF in comments

--- mlf1_1.m
+++ mlf1_1.m
@@ -1,5 +1,18 @@
+
+
+% Measurement should be raw vals from 6 sensors, all the math done here
+%
 function likelihood = mlf1_1(pf, predictParticles, measurement)
     
+    % First map measurements to most-likely radius from each sensor
+    % Get from Andrew
     
+    % Then make distribution around each sensor (relative to origin!)
+    
+    
+    
+    % Sum all distributions on top of each other
+    
+    % Return summation
     
 end
