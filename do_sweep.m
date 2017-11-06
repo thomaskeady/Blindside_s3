213c99cbc76fa2e5aef19878f1adbba978e3e2e1
thomaskeady <thomas.keady@jhu.edu>
2017-11-05 19:58:10 -0500

    Scripts ready to go

--- do_sweep.m
+++ do_sweep.m
@@ -4,13 +4,13 @@ f = 'data/data_outside_10-30-2017_13-48-09-side1_forMat.csv';
 
 myParams = Parameters(f);
 
-gsdR = [0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1, 2, 5, 10];
+gsdR = [0.1, 0.2, 0.3, 0.4, 0.5, 1, 5];
 
 wbdR = [1]; % Doesnt do anything right now
 
-psfR = [0.1 0.5, 0.7, 0.9, 1, 1.5, 2, 3, 5, 10];
+psfR = [0.1, 0.2, 0.5, 1, 2];
 
-npR  = [250, 500, 1000, 3000, 5000, 7500, 10000];
+npR  = [250, 500, 1000, 5000, 10000];
 
 rsmR = ["multinomial", "systematic", "stratified", "residual"];
 %rsmR = ["multinomial"];
--- do_sweep3.m
+++ do_sweep3.m
@@ -1,16 +1,16 @@
 disp('Starting');
 
-f = 'data/data_outside_10-30-2017_14-07-19-front2_forMat.csv';
+f = 'data/live_11-05-2017_13-05-31_2mL_forMat.csv';
 
 myParams = Parameters(f);
 
-gsdR = [0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1, 2, 5, 10];
+gsdR = [0.1, 0.2, 0.3, 0.4, 0.5, 1, 5];
 
 wbdR = [1]; % Doesnt do anything right now
 
-psfR = [0.1 0.5, 0.7, 0.9, 1, 1.5, 2, 3, 5, 10];
+psfR = [0.1, 0.2, 0.5, 1, 2];
 
-npR  = [250, 500, 1000, 3000, 5000, 7500, 10000];
+npR  = [250, 500, 1000, 5000, 10000];
 
 rsmR = ["multinomial", "systematic", "stratified", "residual"];
 %rsmR = ["multinomial"];
